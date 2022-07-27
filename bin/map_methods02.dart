47f736a3b8f33fc88287fa2e0489bcbcf89d8ebe
shohrux <shohruxergashev67@gmail.com>
2022-07-27 16:59:19 +0500

    3280

--- bin/map_methods02.dart
+++ bin/map_methods02.dart
@@ -1,11 +1,12 @@
 /*
 Car details will be given to you in map view, change the car price for it
 */
-
-void main() {}
+void main() {
+  print(func({'name': 'lassette', 'colar': 'silver', 'price': 6000}, 3400));
+}
 
 Map func(Map data, int cost) {
+  data["price"] = cost;
   //your code here
-
   return data;
-}
+} 
