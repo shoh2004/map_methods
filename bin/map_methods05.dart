45606258fa30318742b0da73736b1ceae4f5c991
shohrux <shohruxergashev67@gmail.com>
2022-07-28 09:04:39 +0500

    3280

--- bin/map_methods05.dart
+++ bin/map_methods05.dart
@@ -4,16 +4,15 @@ the key in it is last name,
 change it to surname
 */
 
-
 void main() {
-  print(func({'name': 'laccete', 'shoh_dart': 'silver'}));
+  print(func({'name': 'laccete', 'shoh_name': 'silver'}));
 }
 
 Map func(Map data) {
-  String func = data['shoh_dart'];
-  data.remove('shoh_dart');
+  String func = data['shoh_name'];
+  data.remove('shoh_name');
   data["surname"] = func;
   //your code here
 
-  return data;
-}   
+return data;
+} 
